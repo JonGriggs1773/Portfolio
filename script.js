e37ed1c07f1264dcfb433d14e03ab430f6be4888
Jon Griggs <bigmac_beats1773@outlook.com>
2023-11-16 22:40:45 -0600

    added custom modal, fixed intro some more, and made skill display function more efficient

--- script.js
+++ script.js
@@ -1,39 +1,24 @@
 
+// Variable used for showDetails function
+const contentMap = {
+    "Front-End": "<p>HTML, CSS and Javascript</p>",
+    "Back-End": "<p>Python, Java and MERN</p>",
+    "Databases": "<p>MySQL and MongoDB</p>",
+    "Frameworks/\nLibraries": "<p style='font-size: small; padding: 10px;'>Flask, SpringBoot, libGDX, Jinja2, React.js, Express.js, Chart.js, Three.js, Bootstrap, Mongoose</p>",
+    "Tools": "<p style='font-size: small; padding: 10px;'>VSCode, Eclipse, Vite, Maven, Postman, and Figma</p>",
+    "HTML, CSS and Javascript": "<h3>Front-End</h3>",
+    "Python, Java and MERN": "<h3>Back-End</h3>",
+    "MySQL and MongoDB": "<h3>Databases</h3>",
+    "Flask, SpringBoot, libGDX, Jinja2, React.js, Express.js, Chart.js, Three.js, Bootstrap, Mongoose": "<h3>Frameworks/</h3><h3>Libraries</h3>",
+    "VSCode, Eclipse, Vite, Maven, Postman, and Figma": "<h3>Tools</h3>"
+}
+
+// Variable needed for the custom modal function
+let beenCalled = false
 
 const showDetails = (element) => {
     let text = element.innerText
-    console.log(text)
-    switch(text) {
-        case "Front-End":
-            element.innerHTML = "<p>HTML, CSS and Javascript</p>"
-            break
-        case "Back-End":
-            element.innerHTML = "<p>Python, Java and MERN</p>"
-            break
-        case "Databases":
-            element.innerHTML = "<p>MySQL and MongoDB</p>"
-            break
-        case "Frameworks/\nLibraries":
-            element.innerHTML = "<p style='font-size: small; padding: 10px;'>Flask, SpringBoot, libGDX, Jinja2, React.js, Express.js, Chart.js, Three.js, Bootstrap, Mongoose</p>"
-            break
-        case "Tools":
-            element.innerHTML = "<p style='font-size: small; padding: 10px;'>VSCode, Eclipse, Vite, Maven, Postman, and Figma</p>"
-            break
-        case "HTML, CSS and Javascript":
-            element.innerHTML = "<h3>Front-End</h3>"
-            break
-        case "Python, Java and MERN":
-            element.innerHTML = "<h3>Back-End</h3>"
-            break
-        case "MySQL and MongoDB":
-            element.innerHTML = "<h3>Databases</h3>"
-            break
-        case "Flask, SpringBoot, libGDX, Jinja2, React.js, Express.js, Chart.js, Three.js, Bootstrap, Mongoose":
-            element.innerHTML = "<h3>Frameworks/</h3><h3>Libraries</h3>"
-            break
-        case "VSCode, Eclipse, Vite, Maven, Postman, and Figma":
-            element.innerHTML = "<h3>Tools</h3>"
-    }
+    element.innerHTML = contentMap[text] || text
 }
 
 
@@ -45,3 +30,22 @@ const shadowEffectOn = (element) => {
 const shadowEffectOff = (element) => {
     element.style.boxShadow = ""
 }
+
+const showAlert = () => {
+    document.getElementById("custom-modal").style.display = "block"
+}
+
+const closeAlert = () => {
+    document.getElementById("custom-modal").style.display = "none"
+}
+
+
+const handleAlertBasedOnScrollDistance = () => {
+    let scrollPosition = window.scrollY || document.documentElement.scrollTop
+    if (scrollPosition > 450 && beenCalled == false) {
+        showAlert()
+        beenCalled = true
+    }
+}
+
+window.addEventListener('scroll', handleAlertBasedOnScrollDistance)
